0e8818428fd055fbd5419f625e1a1d311f756c4d
2mer <kenyweb7@gmail.com>
2025-03-31 21:45:13 +0300

    fixed some levels

--- src/components/LevelContent.tsx
+++ src/components/LevelContent.tsx
@@ -4,6 +4,11 @@ import { checkSolution, type Level } from "../game/Level";
 function LevelContent({ level }: { level: Level<any, any> }) {
 	const [exampleRound, setExampleRound] = useState<string>();
 
+	function generateExampleRound() {
+		setExampleRound(
+			JSON.stringify(level.generateRound().entry, undefined, 4),
+		)
+	}
 
 	useEffect(() => {
 		Object.defineProperty(window, "solution", {
@@ -18,6 +23,8 @@ function LevelContent({ level }: { level: Level<any, any> }) {
 			},
 		});
 
+		generateExampleRound();
+
 		return () => {
 			// biome-ignore lint/performance/noDelete: <explanation>
 			delete (window as any).solution;
@@ -25,7 +32,7 @@ function LevelContent({ level }: { level: Level<any, any> }) {
 	}, [level]);
 
 	return (
-		<div className="flex flex-col items-start gap-4 flex-1 h-full">
+		<div className="flex flex-col gap-4 flex-1 h-full items-stretch">
 			<div className="flex gap-4">
 				<p className="text-xl">{level.name}</p>
 			</div>
@@ -35,21 +42,24 @@ function LevelContent({ level }: { level: Level<any, any> }) {
 				level.description
 			)}
 
-			<p>entry</p>
-			<button
-				type="button"
-				onClick={() =>
-					setExampleRound(
-						JSON.stringify(level.generateRound().entry, undefined, 4),
-					)
-				}
-			>
-				generate example entry
-			</button>
-			<pre className="bg-slate-200 rounded-md p-2 w-full flex-1 overflow-auto min-h-0">
-				<code>{exampleRound}</code>
-			</pre>
-		</div>
+			<div className="flex flex-col gap-2 flex-1 h-full items-stretch border-8 border-slate-300 rounded-md bg-slate-300">
+				<div className="flex justify-between items-end">
+					<span className="font-bold text-slate-500 p-2">Example Entry</span>
+					<button
+						type="button"
+						onClick={() => {
+							generateExampleRound()
+						}}
+					>
+						generate example entry
+					</button>
+				</div>
+
+				<pre className="bg-slate-200 rounded-md p-2 w-full flex-1 overflow-auto min-h-0">
+					<code>{exampleRound}</code>
+				</pre>
+			</div>
+		</div >
 	);
 }
 
--- src/game/levels/01004_CreateArrayI.ts
+++ src/game/levels/01004_CreateArrayI.ts
@@ -1,15 +1,18 @@
 import { random } from "radash";
 import { Level, MatchRound } from "../Level";
 
-export default Level({
-	name: 'Create index array',
-	description: 'create array of length `l`, where the value in each index is equal to the index'
-}, () => {
-	const n = random(1, 100);
-	const l = random(1, 100);
+export default Level(
+	{
+		name: "Create index array",
+		description:
+			"create array of length `l`, where the value in each index is equal to the index",
+	},
+	() => {
+		const l = random(1, 100);
 
-	const entry = Array.from({ length: l }, () => 0);
-	const match = entry.map((_, i) => i);
+		const entry = Array.from({ length: l }, () => 0);
+		const match = entry.map((_, i) => i);
 
-	return MatchRound({ entry, n }, match);
-})
+		return MatchRound({ l }, match);
+	},
+);
